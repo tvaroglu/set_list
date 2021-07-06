d9df67b0ab743f43540eb0a74f998163cdc60956
Taylor Varoglu <tava8398@colorado.edu>
2021-07-06 16:15:54 -0600

    Add Songs to routes and controllers

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,7 @@
+class SongsController < ApplicationController
+  #inhereting from ApplicationController is what makes this class a "controller"
+
+  def index
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  ## below is rails-specific syntax for standard routing protocols
+    ## request method to 'name_of_controller'#'action_of_controller' (or 'action_inside_controller')
+  get '/songs', to: 'songs#index'
 end
