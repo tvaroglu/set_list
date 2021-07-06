24742b6729891b5aa9a3928440cd928717a0839d
Taylor Varoglu <tava8398@colorado.edu>
2021-07-06 17:42:47 -0600

    Add Song model

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,8 +2,10 @@ class SongsController < ApplicationController
   #inhereting from ApplicationController is what makes this class a "controller"
 
   def index
-    ## empty method, exists to point controller to corresponding view
+    ## method exists to point controller to corresponding view
       ## corresponding view gets created via app/views/<controller_name>/index.html.erb
+    # must define the call to .all via INSTANCE VARIABLE, or it won't be accessible in the corresponding view via erb tags!
+    @songs = Song.all
   end
 
 end
---
+++ app/models/song.rb
@@ -0,0 +1,6 @@
+class Song < ApplicationRecord
+  ## models are the "connection" to our database
+    ## inhereting from ApplicationRecord is what makes this class a "model"
+  ## class file should be named in the singular (per standard Ruby convention)
+
+end
